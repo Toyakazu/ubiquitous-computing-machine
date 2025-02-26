c1649e52ff5149a15e8d69466691d9e566f3974f
Matthew Williams <113633993+Toyakazu@users.noreply.github.com>
2025-02-26 08:01:50 -0500

    Add files via upload

--- algorithms hw3/run.bat
+++ algorithms hw3/run.bat
@@ -1,20 +1,37 @@
-@echo on
+@echo off
 setlocal
 
-:: Change directory to the location of the Java files
-cd lox
+:: Set the package directory
+set PACKAGE_DIR=lox
 
+:: Make sure we're in the right directory
+cd %~dp0
 
-:: Compile Java files
+:: Clean previous compilation
+echo Cleaning previous compilation...
+del /Q %PACKAGE_DIR%\*.class 2>nul
+
+:: Compile all Java files
 echo Compiling Java files...
-javac *.java
+javac %PACKAGE_DIR%\*.java
 
-:: Run the main class (replace 'Lox' with your actual main class name if different)
-echo Running the application...
-java Lox.class
+:: Check if compilation was successful
+if %ERRORLEVEL% NEQ 0 (
+    echo Compilation failed!
+    pause
+    exit /b 1
+)
 
-echo Operation complete.
+:: Run the Lox interpreter
+echo Running Lox interpreter...
+java lox.Lox %*
+
+:: If additional arguments were passed, exit
+if not "%~1"=="" (
+    exit /b 0
+)
+
+:: Otherwise pause to see the output
+echo.
+echo Execution complete.
 pause
-
-:: Change back to the original directory
-cd ..\..
